a67e3c849ecc338ee2193ef4b2e8a37bd0a10cdb
cdc192728 <diazcalderincristian113@gmail.com>
2025-02-26 10:50:08 -0500

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,23 @@
+import AboutMe from "@/components/about-me";
+import Contact from "@/components/contact";
+import Experience from "@/components/experience";
+import Footer from "@/components/footer";
+import Introduction from "@/components/Introduction";
+import Navbar from "@/components/navbar";
+import Portfolio from "@/components/portfolio";
+import Services from "@/components/services";
+
+export default function Home() {
+  return (
+    <main className="pb-40">
+      <Navbar />
+      <Introduction />
+      <AboutMe />
+      <Experience />
+      <Services />
+      <Portfolio />
+      <Contact />
+      <Footer />
+    </main>
+  );
+}
