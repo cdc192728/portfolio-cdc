ee2855492cf38c740ff18c7f1620bf14922c4fc4
Cristian Diaz Calderin <diazcalderincristian113@gmail.com>
2025-07-23 13:09:58 -0500

    second commit

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -1,29 +1,29 @@
 // re_RDugdJFC_J5F6SLvMBdWN5KVwa7NwwB1B
 import { Resend } from "resend";
-import { EmailTemplate } from "@/components/email-template";
+// import { EmailTemplate } from "@/components/email-template";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+// const resend = new Resend(process.env.RESEND_API_KEY);
 
-export async function POST(req: Request) {
-  try {
-    const dataForm = await req.json();
-    try {
-      const data = await resend.emails.send({
-        from: "CDC <onboarding@resend.dev>",
-        to: ["diazcalderincristian113@gmail.com"],
-        subject: "CDC landing",
-        react: EmailTemplate({
-          firstName: dataForm.username,
-          message: dataForm.message,
-          email: dataForm.email,
-        }),
-        text: "CDC",
-      });
-      return Response.json(data);
-    } catch (error) {
-      return Response.json({ error });
-    }
-  } catch (error) {
-    return Response.json({ error });
-  }
-}
+// export async function POST(req: Request) {
+//   try {
+//     const dataForm = await req.json();
+//     try {
+//       const data = await resend.emails.send({
+//         from: "CDC <onboarding@resend.dev>",
+//         to: ["diazcalderincristian113@gmail.com"],
+//         subject: "CDC landing",
+//         react: EmailTemplate({
+//           firstName: dataForm.username,
+//           message: dataForm.message,
+//           email: dataForm.email,
+//         }),
+//         text: "CDC",
+//       });
+//       return Response.json(data);
+//     } catch (error) {
+//       return Response.json({ error });
+//     }
+//   } catch (error) {
+//     return Response.json({ error });
+//   }
+// }
